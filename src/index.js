b1004e4e424645e38645a808b499d7deec29672e
Mateo Arboleda <marteomasoyyo@gmail.com>
2019-01-03 21:21:48 -0500

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (id) => links.filter(val => val.id === id)[0]
+  },
+  Mutation: {
+    postLink: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    },
+    updateLink: (parent, args) => {
+      links.filter(val => val.id === id)[0]
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
