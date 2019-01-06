fb6154e642220e494f23f4860b5d25b3d3c47651
Mateo Arboleda <marteomasoyyo@gmail.com>
2019-01-06 11:06:40 -0500

    Resolvers adapted to prisma

--- src/index.js
+++ src/index.js
@@ -1,57 +1,26 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (id) => {
-      return {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL'
-      }
+    feed: () => (root, args, context, info) => {
+      return context.prisma.links()
     }
   },
   Mutation: {
-    postLink: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    postLink: (parent, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      })
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma }
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
-
-async function main() {
-
-  // Create a new link
-  const newLink = await prisma.createLink({ 
-    url: 'www.prisma.io',
-    description: 'Prisma replaces traditional ORMs',
-  })
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`)
-
-  // Read all links from the database and print them to the console
-  const allLinks = await prisma.links()
-  console.log(allLinks)
-}
-
-main().catch(e => console.error(e))
